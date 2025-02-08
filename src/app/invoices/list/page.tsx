fa5d4cddd07f42fc74bd0989629e3f1c8266f516
Hafizh <hahafizhmaulana@gmail.com>
2025-02-08 19:03:59 +0700

    fix: route error

--- src/app/invoices/list/page.tsx
+++ src/app/invoices/list/page.tsx
@@ -1,8 +1,12 @@
 import ListInvoice from "@/app/components/invoices/ListInvoice";
-import React from "react";
+import React, { Suspense } from "react";
 
 const List = () => {
-  return <ListInvoice />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ListInvoice />
+    </Suspense>
+  );
 };
 
 export default List;
